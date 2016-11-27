323c51a94d30033a0b97c3e90d4d3c6751fb327c
nate <ntrusse2@illinois.edu>
2016-11-26 19:02:32 -0600

    Readme describes what features i want to add to the project

---
+++ MC/taia.py
@@ -0,0 +1,14 @@
+import time
+
+#import numpy as np
+#import scipy
+#import pandas as pd
+from scipy.signal import convolve, gaussian
+# from sklearn import linear_model
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+
+for i in range(100):
+    print('TAIA')
+    time.sleep(100)
---
+++ build_app.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    import subprocess
+    subprocess.call(['pyinstaller','class.py'])
